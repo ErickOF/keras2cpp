a130c9dc202634f5610d720bbcc2046240c075b0
Erick Andrés Obregón Fonseca <erickobregonf@gmail.com>
2022-04-11 23:00:49 -0600

    [Update] Adding support for TensorFlow 2.8 and Python 3.9
    - Originally worked with Python 2.7, but was updated to work with Python 3 (tested with Python 3.9 only)
    - dump_to_simple_cpp.py was modified to use the TensorFlow 2.8 model architecture, adding the Input and activation layers.
    - Output data was formatted to use the TensorFlow 2.8 input files
    - Reading until "End" layer is found.
    - Adding input layer.
    - Adding bias in Convolution 2D layer.
    - Adding verbose feature.
    - Formatting code.

--- dump_to_simple_cpp.py
+++ dump_to_simple_cpp.py
@@ -1,72 +1,117 @@
-import numpy as np
-np.random.seed(1337)
-from keras.models import Sequential, model_from_json
-import json
 import argparse
+import json
+from keras.models import model_from_json
+import numpy as np
 
-np.set_printoptions(threshold=np.inf)
-parser = argparse.ArgumentParser(description='This is a simple script to dump Keras model into simple format suitable for porting into pure C++ model')
 
-parser.add_argument('-a', '--architecture', help="JSON with model architecture", required=True)
-parser.add_argument('-w', '--weights', help="Model weights in HDF5 format", required=True)
-parser.add_argument('-o', '--output', help="Ouput file name", required=True)
-parser.add_argument('-v', '--verbose', help="Verbose", required=False)
-args = parser.parse_args()
+def convert_model(args):
+    """Convert input JSON file to a keras model.
 
-print 'Read architecture from', args.architecture
-print 'Read weights from', args.weights
-print 'Writing to', args.output
+    Args:
+        args (Sequence[str]): parsed arguments from console.
+    """
+    # Read the NN architecture
+    arch = open(args.architecture).read()
+    # Parsing the model from JSON format
+    model = model_from_json(arch)
+    # Load weights in H5 format.
+    model.load_weights(args.weights)
+    # Compile the Keras model
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta')
+    # Get JSON file as a Python dictionary
+    arch = json.loads(arch)
 
-arch = open(args.architecture).read()
-model = model_from_json(arch)
-model.load_weights(args.weights)
-model.compile(loss='categorical_crossentropy', optimizer='adadelta')
-arch = json.loads(arch)
+    # Empty the content of the model into an output file used by keras2cpp
+    with open(args.output, 'w') as fout:
+        fout.write(f'layers {len(model.layers)}\n')
 
-with open(args.output, 'w') as fout:
-    fout.write('layers ' + str(len(model.layers)) + '\n')
+        layers = []
+        activations = 0
 
-    layers = []
-    for ind, l in enumerate(arch["config"]):
-        if args.verbose:
-            print ind, l
-        fout.write('layer ' + str(ind) + ' ' + l['class_name'] + '\n')
-
-        if args.verbose:
-            print str(ind), l['class_name']
-        layers += [l['class_name']]
-        if l['class_name'] == 'Convolution2D':
-            #fout.write(str(l['config']['nb_filter']) + ' ' + str(l['config']['nb_col']) + ' ' + str(l['config']['nb_row']) + ' ')
-
-            #if 'batch_input_shape' in l['config']:
-            #    fout.write(str(l['config']['batch_input_shape'][1]) + ' ' + str(l['config']['batch_input_shape'][2]) + ' ' + str(l['config']['batch_input_shape'][3]))
-            #fout.write('\n')
-
-            W = model.layers[ind].get_weights()[0]
+        for ind, l in enumerate(arch['config']['layers']):
             if args.verbose:
-                print W.shape
-            fout.write(str(W.shape[0]) + ' ' + str(W.shape[1]) + ' ' + str(W.shape[2]) + ' ' + str(W.shape[3]) + ' ' + l['config']['border_mode'] + '\n')
+                print(ind + activations, l)
 
-            for i in range(W.shape[0]):
-                for j in range(W.shape[1]):
-                    for k in range(W.shape[2]):
-                        fout.write(str(W[i,j,k]) + '\n')
-            fout.write(str(model.layers[ind].get_weights()[1]) + '\n')
+            fout.write(f'layer {ind + activations} {l["class_name"]}\n')
 
-        if l['class_name'] == 'Activation':
-            fout.write(l['config']['activation'] + '\n')
-        if l['class_name'] == 'MaxPooling2D':
-            fout.write(str(l['config']['pool_size'][0]) + ' ' + str(l['config']['pool_size'][1]) + '\n')
-        #if l['class_name'] == 'Flatten':
-        #    print l['config']['name']
-        if l['class_name'] == 'Dense':
-            #fout.write(str(l['config']['output_dim']) + '\n')
-            W = model.layers[ind].get_weights()[0]
-            if args.verbose:
-                print W.shape
-            fout.write(str(W.shape[0]) + ' ' + str(W.shape[1]) + '\n')
+            print(f'{ind + activations} {l["class_name"]}')
 
+            layers += [l['class_name']]
 
-            for w in W:
-                fout.write(str(w) + '\n')
-            fout.write(str(model.layers[ind].get_weights()[1]) + '\n')
+            if l['class_name'] == 'Conv2D':
+                #fout.write(str(l['config']['nb_filter']) + ' ' + str(l['config']['nb_col']) + ' ' + str(l['config']['nb_row']) + ' ')
+
+                # if 'batch_input_shape' in l['config']:
+                #    fout.write(str(l['config']['batch_input_shape'][1]) + ' ' + str(l['config']['batch_input_shape'][2]) + ' ' + str(l['config']['batch_input_shape'][3]))
+                # fout.write('\n')
+
+                W = model.layers[ind].get_weights()[0]
+                w_rows, w_cols, w_depth, w_batch = W.shape
+                W = W.reshape((w_batch, w_depth, w_rows, w_cols))
+
+                if args.verbose:
+                    print(W.shape)
+
+                fout.write(f'{W.shape[0]} {W.shape[1]} {W.shape[2]} {W.shape[3]} {l["config"]["padding"]}\n')
+
+                for i in range(W.shape[0]):
+                    for j in range(W.shape[1]):
+                        for k in range(W.shape[2]):
+                            fout.write(str(W[i, j, k]) + '\n')
+
+                fout.write(f'{model.layers[ind].get_weights()[1]}\n')
+
+            elif l['class_name'] == 'MaxPooling2D':
+                fout.write(f'{l["config"]["pool_size"][0]} {l["config"]["pool_size"][1]}\n')
+            # if l['class_name'] == 'Flatten':
+            #    print(l['config']['name'])
+
+            elif l['class_name'] == 'Dense':
+                # fout.write(str(l['config']['output_dim']) + '\n')
+                W = model.layers[ind].get_weights()[0]
+
+                if args.verbose:
+                    print(W.shape)
+
+                fout.write(f'{W.shape[0]} {W.shape[1]}\n')
+
+                for w in W:
+                    fout.write(f'{w}\n')
+
+                fout.write(f'{model.layers[ind].get_weights()[1]}\n')
+
+            if 'activation' in l['config']:
+                activations += 1
+                print(f'{ind + activations} Activation')
+                fout.write(f'layer {ind + activations} Activation\n')
+                fout.write(f'{l["config"]["activation"]}\n')
+
+                if args.verbose:
+                    print(l["config"]["activation"])
+
+        fout.write(f'layer {ind + activations} End\n')
+        print(f'{ind + activations} End')
+
+if __name__ == '__main__':
+    np.random.seed(1337)
+    np.set_printoptions(threshold=np.inf)
+
+    # Parsing console arguments
+    parser = argparse.ArgumentParser(
+        description='This is a simple script to dump Keras model into simple format suitable for porting into pure C++ model')
+
+    parser.add_argument('-a', '--architecture',
+                        help="JSON with model architecture", required=True)
+    parser.add_argument('-w', '--weights',
+                        help="Model weights in HDF5 format", required=True)
+    parser.add_argument(
+        '-o', '--output', help="Ouput file name", required=True)
+    parser.add_argument('-v', '--verbose', help="Verbose", required=False)
+    args = parser.parse_args()
+    args.verbose = args.verbose == '1'
+
+    print(f'Read architecture from {args.architecture}')
+    print(f'Read weights from {args.weights}')
+    print(f'Writing to {args.output}')
+
+    convert_model(args)
--- keras_model.h
+++ keras_model.h
@@ -8,101 +8,174 @@
 
 namespace keras
 {
-	std::vector<float> read_1d_array(std::ifstream &fin, int cols);
-	void missing_activation_impl(const std::string &act);
-	std::vector< std::vector<float> > conv_single_depth_valid(std::vector< std::vector<float> > const & im, std::vector< std::vector<float> > const & k);
-	std::vector< std::vector<float> > conv_single_depth_same(std::vector< std::vector<float> > const & im, std::vector< std::vector<float> > const & k);
+  std::vector<float> read_1d_array(std::ifstream &fin, int cols);
+  void missing_activation_impl(const std::string &act);
+  std::vector<std::vector<float>> conv_single_depth_valid(std::vector<std::vector<float>> const &im, std::vector<std::vector<float>> const &k);
+  std::vector<std::vector<float>> conv_single_depth_same(std::vector<std::vector<float>> const &im, std::vector<std::vector<float>> const &k);
 
-	class DataChunk;
-	class DataChunk2D;
-	class DataChunkFlat;
+  class DataChunk;
+  class DataChunk2D;
+  class DataChunkFlat;
 
-	class Layer;
-	class LayerFlatten;
-	class LayerMaxPooling;
-	class LayerActivation;
-	class LayerConv2D;
-	class LayerDense;
+  class Layer;
+  class LayerFlatten;
+  class LayerMaxPooling;
+  class LayerActivation;
+  class LayerConv2D;
+  class LayerDense;
+  class LayerInput;
 
-	class KerasModel;
+  class KerasModel;
 }
 
-class keras::DataChunk {
+class keras::DataChunk
+{
 public:
   virtual ~DataChunk() {}
-  virtual size_t get_data_dim(void) const { return 0; }
-  virtual std::vector<float> const & get_1d() const { throw "not implemented"; };
-  virtual std::vector<std::vector<std::vector<float> > > const & get_3d() const { throw "not implemented"; };
-  virtual void set_data(std::vector<std::vector<std::vector<float> > > const &) {};
+
+  virtual size_t get_data_dim(void) const
+  {
+    return 0;
+  }
+
+  virtual std::vector<float> const &get_1d() const
+  {
+    throw "not implemented";
+  };
+
+  virtual std::vector<std::vector<std::vector<float>>> const &get_3d() const
+  {
+    throw "not implemented";
+  };
+
+  virtual void set_data(std::vector<std::vector<std::vector<float>>> const &) {};
+
   virtual void set_data(std::vector<float> const &) {};
-  //virtual unsigned int get_count();
+  // virtual unsigned int get_count();
   virtual void read_from_file(const std::string &fname) {};
   virtual void show_name() = 0;
   virtual void show_values() = 0;
 };
 
-class keras::DataChunk2D : public keras::DataChunk {
+class keras::DataChunk2D : public keras::DataChunk
+{
 public:
-  std::vector< std::vector< std::vector<float> > > const & get_3d() const { return data; };
-  virtual void set_data(std::vector<std::vector<std::vector<float> > > const & d) { data = d; };
-  size_t get_data_dim(void) const { return 3; }
+  DataChunk2D(bool verbose) : DataChunk() {
+    this->verbose = verbose;
+  };
 
-  void show_name() {
+  DataChunk2D() : DataChunk() {};
+
+  void flatten();
+
+  std::vector<float> const &get_1d() const
+  {
+    return flat_out;
+  };
+
+  std::vector<std::vector<std::vector<float>>> const &get_3d() const
+  {
+    return data;
+  };
+
+  virtual void set_data(std::vector<std::vector<std::vector<float>>> const &d) {
+    data = d;
+  };
+
+  size_t get_data_dim(void) const
+  {
+    return 3;
+  }
+
+  void show_name()
+  {
     std::cout << "DataChunk2D " << data.size() << "x" << data[0].size() << "x" << data[0][0].size() << std::endl;
   }
 
-  void show_values() {
+  void show_values()
+  {
     std::cout << "DataChunk2D values:" << std::endl;
-    for(size_t i = 0; i < data.size(); ++i) {
+
+    for (size_t i = 0; i < data.size(); ++i)
+    {
       std::cout << "Kernel " << i << std::endl;
-      for(size_t j = 0; j < data[0].size(); ++j) {
-        for(size_t k = 0; k < data[0][0].size(); ++k) {
+
+      for (size_t j = 0; j < data[0].size(); ++j)
+      {
+        for (size_t k = 0; k < data[0][0].size(); ++k)
+        {
           std::cout << data[i][j][k] << " ";
         }
+
         std::cout << std::endl;
       }
     }
   }
-  //unsigned int get_count() {
-  //  return data.size()*data[0].size()*data[0][0].size();
-  //}
+  // unsigned int get_count() {
+  //   return data.size()*data[0].size()*data[0][0].size();
+  // }
 
   void read_from_file(const std::string &fname);
-  std::vector<std::vector<std::vector<float> > > data; // depth, rows, cols
 
+  std::vector<std::vector<std::vector<float>>> data; // depth, rows, cols
+  std::vector<float> flat_out;
+
+  bool verbose;
   int m_depth;
   int m_rows;
   int m_cols;
 };
 
-class keras::DataChunkFlat : public keras::DataChunk {
+class keras::DataChunkFlat : public keras::DataChunk
+{
 public:
-  DataChunkFlat(size_t size) : f(size) { }
-  DataChunkFlat(size_t size, float init) : f(size, init) { }
-  DataChunkFlat(void) { }
+  DataChunkFlat(size_t size) : f(size) {}
+  DataChunkFlat(size_t size, float init) : f(size, init) {}
+  DataChunkFlat(void) {}
 
   std::vector<float> f;
-  std::vector<float> & get_1d_rw() { return f; }
-  std::vector<float> const & get_1d() const { return f; }
-  void set_data(std::vector<float> const & d) { f = d; };
-  size_t get_data_dim(void) const { return 1; }
+  std::vector<float> &get_1d_rw() {
+    return f;
+  }
 
-  void show_name() {
+  std::vector<float> const &get_1d() const
+  {
+    return f;
+  }
+
+  void set_data(std::vector<float> const &d) {
+    f = d;
+  };
+
+  size_t get_data_dim(void) const
+  {
+    return 1;
+  }
+
+  void show_name()
+  {
     std::cout << "DataChunkFlat " << f.size() << std::endl;
   }
-  void show_values() {
+
+  void show_values()
+  {
     std::cout << "DataChunkFlat values:" << std::endl;
-    for(size_t i = 0; i < f.size(); ++i) std::cout << f[i] << " ";
+
+    for (size_t i = 0; i < f.size(); ++i)
+      std::cout << f[i] << " ";
+
     std::cout << std::endl;
   }
+
   void read_from_file(const std::string &fname) {};
-  //unsigned int get_count() { return f.size(); }
+  // unsigned int get_count() { return f.size(); }
 };
 
-class keras::Layer {
+class keras::Layer
+{
 public:
   virtual void load_weights(std::ifstream &fin) = 0;
-  virtual keras::DataChunk* compute_output(keras::DataChunk*) = 0;
+  virtual keras::DataChunk *compute_output(keras::DataChunk *) = 0;
 
   Layer(std::string name) : m_name(name) {}
   virtual ~Layer() {}
@@ -111,64 +184,128 @@ public:
   virtual unsigned int get_input_cols() const = 0;
   virtual unsigned int get_output_units() const = 0;
 
-  std::string get_name() { return m_name; }
+  std::string get_name() {
+    return m_name;
+  }
+
   std::string m_name;
 };
 
-
-class keras::LayerFlatten : public Layer {
+class keras::LayerFlatten : public Layer
+{
 public:
+  LayerFlatten(bool verbose) : Layer("Flatten") {
+    this->verbose = verbose;
+  }
+
   LayerFlatten() : Layer("Flatten") {}
   void load_weights(std::ifstream &fin) {};
-  keras::DataChunk* compute_output(keras::DataChunk*);
+  keras::DataChunk *compute_output(keras::DataChunk *);
 
-  virtual unsigned int get_input_rows() const { return 0; } // look for the value in the preceding layer
-  virtual unsigned int get_input_cols() const { return 0; } // same as for rows
-  virtual unsigned int get_output_units() const { return 0; }
+  virtual unsigned int get_input_rows() const
+  {
+    return 0;
+  } // look for the value in the preceding layer
+
+  virtual unsigned int get_input_cols() const
+  {
+    return 0;
+  } // same as for rows
+
+  virtual unsigned int get_output_units() const
+  {
+    return 0;
+  }
+
+  bool verbose;
 };
 
-
-class keras::LayerMaxPooling : public Layer {
+class keras::LayerMaxPooling : public Layer
+{
 public:
+  LayerMaxPooling(bool verbose) : Layer("MaxPooling2D") {
+    this->verbose = verbose;
+  };
+
   LayerMaxPooling() : Layer("MaxPooling2D") {};
 
   void load_weights(std::ifstream &fin);
-  keras::DataChunk* compute_output(keras::DataChunk*);
+  keras::DataChunk *compute_output(keras::DataChunk *);
 
-  virtual unsigned int get_input_rows() const { return 0; } // look for the value in the preceding layer
-  virtual unsigned int get_input_cols() const { return 0; } // same as for rows
-  virtual unsigned int get_output_units() const { return 0; }
+  virtual unsigned int get_input_rows() const
+  {
+    return 0;
+  } // look for the value in the preceding layer
 
+  virtual unsigned int get_input_cols() const
+  {
+    return 0;
+  } // same as for rows
+
+  virtual unsigned int get_output_units() const
+  {
+    return 0;
+  }
+
+  bool verbose;
   int m_pool_x;
   int m_pool_y;
-
 };
 
-class keras::LayerActivation : public Layer {
+class keras::LayerActivation : public Layer
+{
 public:
   LayerActivation() : Layer("Activation") {}
   void load_weights(std::ifstream &fin);
-  keras::DataChunk* compute_output(keras::DataChunk*);
+  keras::DataChunk *compute_output(keras::DataChunk *);
 
-  virtual unsigned int get_input_rows() const { return 0; } // look for the value in the preceding layer
-  virtual unsigned int get_input_cols() const { return 0; } // same as for rows
-  virtual unsigned int get_output_units() const { return 0; }
+  virtual unsigned int get_input_rows() const
+  {
+    return 0;
+  } // look for the value in the preceding layer
+
+  virtual unsigned int get_input_cols() const
+  {
+    return 0;
+  } // same as for rows
+
+  virtual unsigned int get_output_units() const
+  {
+    return 0;
+  }
 
   std::string m_activation_type;
 };
 
-class keras::LayerConv2D : public Layer {
+class keras::LayerConv2D : public Layer
+{
 public:
+  LayerConv2D(bool verbose) : Layer("Conv2D") {
+    this->verbose = verbose;
+  }
+
   LayerConv2D() : Layer("Conv2D") {}
 
   void load_weights(std::ifstream &fin);
-  keras::DataChunk* compute_output(keras::DataChunk*);
-  std::vector<std::vector<std::vector<std::vector<float> > > > m_kernels; // kernel, depth, rows, cols
-  std::vector<float> m_bias; // kernel
+  keras::DataChunk *compute_output(keras::DataChunk *);
+  std::vector<std::vector<std::vector<std::vector<float>>>> m_kernels; // kernel, depth, rows, cols
+  std::vector<float> m_bias;                                           // kernel
+  bool verbose;
 
-  virtual unsigned int get_input_rows() const { return m_rows; }
-  virtual unsigned int get_input_cols() const { return m_cols; }
-  virtual unsigned int get_output_units() const { return m_kernels_cnt; }
+  virtual unsigned int get_input_rows() const
+  {
+    return m_rows;
+  }
+
+  virtual unsigned int get_input_cols() const
+  {
+    return m_cols;
+  }
+
+  virtual unsigned int get_output_units() const
+  {
+    return m_kernels_cnt;
+  }
 
   std::string m_border_mode;
   int m_kernels_cnt;
@@ -177,40 +314,91 @@ public:
   int m_cols;
 };
 
-class keras::LayerDense : public Layer {
+class keras::LayerDense : public Layer
+{
 public:
+  LayerDense(bool verbose) : Layer("Dense") {
+    this->verbose = verbose;
+  }
+
   LayerDense() : Layer("Dense") {}
 
   void load_weights(std::ifstream &fin);
-  keras::DataChunk* compute_output(keras::DataChunk*);
-  std::vector<std::vector<float> > m_weights; //input, neuron
-  std::vector<float> m_bias; // neuron
+  keras::DataChunk *compute_output(keras::DataChunk *);
+  std::vector<std::vector<float>> m_weights; // input, neuron
+  std::vector<float> m_bias;                 // neuron
 
-  virtual unsigned int get_input_rows() const { return 1; } // flat, just one row
-  virtual unsigned int get_input_cols() const { return m_input_cnt; }
-  virtual unsigned int get_output_units() const { return m_neurons; }
+  virtual unsigned int get_input_rows() const
+  {
+    return 1;
+  } // flat, just one row
+
+  virtual unsigned int get_input_cols() const
+  {
+    return m_input_cnt;
+  }
+
+  virtual unsigned int get_output_units() const
+  {
+    return m_neurons;
+  }
+
+  bool verbose;
+  int m_input_cnt;
+  int m_neurons;
+};
+
+class keras::LayerInput : public Layer
+{
+public:
+  LayerInput() : Layer("Input") {}
+  void load_weights(std::ifstream &fin);
+  keras::DataChunk *compute_output(keras::DataChunk *);
+  std::vector<std::vector<float>> m_weights;
+
+  virtual unsigned int get_input_rows() const
+  {
+    return 1;
+  } // flat, just one row
+
+  virtual unsigned int get_input_cols() const
+  {
+    return m_input_cnt;
+  }
+
+  virtual unsigned int get_output_units() const
+  {
+    return m_neurons;
+  }
 
   int m_input_cnt;
   int m_neurons;
 };
 
-class keras::KerasModel {
+class keras::KerasModel
+{
 public:
   KerasModel(const std::string &input_fname, bool verbose);
   ~KerasModel();
   std::vector<float> compute_output(keras::DataChunk *dc);
 
-  unsigned int get_input_rows() const { return m_layers.front()->get_input_rows(); }
-  unsigned int get_input_cols() const { return m_layers.front()->get_input_cols(); }
+  unsigned int get_input_rows() const
+  {
+    return m_layers.front()->get_input_rows();
+  }
+
+  unsigned int get_input_cols() const
+  {
+    return m_layers.front()->get_input_cols();
+  }
+
   int get_output_length() const;
 
 private:
-
   void load_weights(const std::string &input_fname);
-  int m_layers_cnt; // number of layers
+  int m_layers_cnt;              // number of layers
   std::vector<Layer *> m_layers; // container with layers
   bool m_verbose;
-
 };
 
 #endif
